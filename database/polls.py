e996dc191b38fae78d3277d86e91de7ad0811793
aNOOBis <69043738+aNOOBisTheGod@users.noreply.github.com>
2022-03-31 20:36:36 +0300

    here is the poll results

--- database/polls.py
+++ database/polls.py
@@ -36,25 +36,45 @@ def retreivePoll(db_sess, id):
     poll = db_sess.query(Poll).filter(Poll.id == id).first()
     return poll
 
+def getChartUrl(poll):
+    poll = ast.literal_eval(poll.options)
+    # url = "https://image-charts.com/chart?chco=000000%2C8AC1E7%7C7f3f98%7Cfdb45c&chs=300x300&cht=bvs&chxt=x%2Cy&chd=a%3A10%2C50%2C60%2C80%2C40&chxl=0:|123|321|213|"
+    # url = "https://image-charts.com/chart?chco=000000%2C8AC1E7%7C7f3f98%7Cfdb45c&chs=300x300&cht=bvs&chxt=x%2Cy&chd=a%3A0%2C0%2C0&chxl=0:|First clause|Second clause|Third clause|"
+    url = "https://image-charts.com/chart?chco=000000%2C8AC1E7%7C7f3f98%7Cfdb45c&chs=500x500&cht=bvs&chxt=x%2Cy&"
+    first = True
+    chd = "chd=a"
+    chxl = "chxl=0:|"
+    for key, value in poll.items():
+        chxl += key.replace(" ", "_") + "|"
+        if first:
+            first = not first
+            chd += "%" + "3A" + str(value)
+        else:
+            chd += "%" + "2C" + str(value)
+    url += chd + "&" + chxl
+    return url
+    
+    
+
 def getPollJson(db_sess, id):
     poll = retreivePoll(db_sess, id)
     return {
         'title': poll.title,
         'options': ast.literal_eval(poll.options),
         'isUnique': poll.unique,
-        'whoVoted': list(map(int, poll.whoVoted.split()))
+        'whoVoted': list(map(int, poll.whoVoted.split())),
+        'chart_url': getChartUrl(poll)
     }
     
     
-def voteInPoll(db_sess, pollId, uid, options):
+def voteInPoll(db_sess, pollId, uid, answer_options):
     poll = retreivePoll(db_sess, pollId)
     votesList = poll.whoVoted
     votesList = votesList.split()
     votesList.append(uid)
-    print(votesList)
     votesList = ' '.join(list(map(str, votesList)))
     options = ast.literal_eval(poll.options)
-    for option in options:
+    for option in answer_options:
         options[option] += 1
     poll.options = str(options)
     poll.whoVoted = votesList
